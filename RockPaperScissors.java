5a2bde758a361b12167d20a0a1e403a58735f0f8
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2020-12-31 13:38:09 -0500

    Pose Question for user

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -6,4 +6,9 @@ public class RockPaperScissors {
   public static void main(String[] args) {
     
     boolean replay = false;
+    while (replay == false) {
+
+      Scanner userInput = new Scanner(System.in);
+      
+      System.out.println("Choose rock(1), paper(2), or scissors(3) to play against the computer.");
 }
